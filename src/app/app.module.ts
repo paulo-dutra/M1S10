43fc0b64132515a55c4ff1ca62c425a5e84d9ab0
Pdutra <powdutra26@gmail.com>
2023-01-12 00:30:00 -0300

    Exercicio 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +8,37 @@ import { HeaderComponent } from './components/header/header.component';
 import { BotoesFiltroComponent } from './components/botoes-filtro/botoes-filtro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home'
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent,
+    children:[
+      {
+        path:'todos',
+        component: HomeComponent
+      },
+      {
+        path:'nao-lidos',
+        component: HomeComponent
+      },
+      {
+        path:'lidos',
+        component: HomeComponent
+      }
+    ]
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +51,7 @@ import { AboutComponent } from './pages/about/about.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
